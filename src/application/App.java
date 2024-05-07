1da9dcaae58a6e63f9659b635781f89d1b246386
Tiam-Rietsch <NgnotchoueTiam@gmail.com>
2024-05-07 10:09:19 +0100

    initial structure

--- src/application/App.java
+++ src/application/App.java
@@ -1,7 +1,18 @@
-package application; 
+package application;
+
+import java.util.Scanner;
 
 public class App {
+    private static Scanner in = new Scanner(System.in);
+    private static String command;
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        do {
+            System.out.print("\n$ ");
+            command = in.nextLine();
+            if (command.equals("exit")) {
+                return;
+            }
+        } while (true);
     }
 }
