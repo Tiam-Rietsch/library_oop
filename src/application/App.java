62aaf1679701d4890c8a019dfe7858f5fb1975ce
Tiam-Rietsch <NgnotchoueTiam@gmail.com>
2024-05-07 13:48:55 +0100

    view table display done

--- src/application/App.java
+++ src/application/App.java
@@ -1,14 +1,32 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Scanner;
+
+import view.View;
 
 public class App {
     private static Scanner in = new Scanner(System.in);
     private static String command;
     
     public static void main(String[] args) throws Exception {
+        String[] list = {"hello", "world"};
+
+        // this is a test table to test if the View class is working
+        LinkedHashMap<String, ArrayList<String>> table = new LinkedHashMap<>() {{
+            put("columnA", new ArrayList(Arrays.asList("cellA1", "cellA2", "cellA3")));
+            put("columnB", new ArrayList(Arrays.asList("cellB1", "cellB2", "cellB3")));
+            put("columnC", new ArrayList(Arrays.asList("cellC1", "cellC2", "cellC3")));
+            put("columnD", new ArrayList(Arrays.asList("cellD1", "cellD2", "cellD3")));
+            put("columnE", new ArrayList(Arrays.asList("cellE1", "cellE2", "cellE3")));
+            put("columnF", new ArrayList(Arrays.asList("cellF1", "cellF2", "cellF3")));
+        }};
         do {
-            System.out.print("\n$ ");
+            // display the fake table
+            View.displayTable("test table", table);
             command = in.nextLine();
             if (command.equals("exit")) {
                 return;
